aa1fb5fef82a3930589b90d17b3100b568d51497
Carlos Eduardo Pinho <carloso.dev@gmail.com>
2025-03-06 23:27:17 -0300

    feat: implement delete transaction functionality with confirmation dialog and toast notifications

--- app/_data/get-dashboard/index.ts
+++ app/_data/get-dashboard/index.ts
@@ -8,7 +8,6 @@ export const getDashboard = async (month: string) => {
   if (!userId) {
     throw new Error("Unauthorized");
   }
-
   const where = {
     userId,
     date: {
@@ -16,57 +15,50 @@ export const getDashboard = async (month: string) => {
       lt: new Date(`2024-${month}-31`),
     },
   };
-
   const depositsTotal = Number(
     (
       await db.transaction.aggregate({
         where: { ...where, type: "DEPOSIT" },
         _sum: { amount: true },
       })
-    )?._sum?.amount || 0,
+    )?._sum?.amount,
   );
-
   const investmentsTotal = Number(
     (
       await db.transaction.aggregate({
         where: { ...where, type: "INVESTMENT" },
         _sum: { amount: true },
       })
-    )?._sum?.amount || 0,
+    )?._sum?.amount,
   );
-
   const expensesTotal = Number(
     (
       await db.transaction.aggregate({
         where: { ...where, type: "EXPENSE" },
         _sum: { amount: true },
       })
-    )?._sum?.amount || 0,
+    )?._sum?.amount,
   );
-
   const balance = depositsTotal - investmentsTotal - expensesTotal;
-
   const transactionsTotal = Number(
     (
       await db.transaction.aggregate({
         where,
         _sum: { amount: true },
       })
-    )?._sum?.amount || 0,
+    )._sum.amount,
   );
-
   const typesPercentage: TransactionPercentagePerType = {
-    [TransactionType.DEPOSIT]: transactionsTotal
-      ? Math.round((depositsTotal / transactionsTotal) * 100)
-      : 0,
-    [TransactionType.EXPENSE]: transactionsTotal
-      ? Math.round((expensesTotal / transactionsTotal) * 100)
-      : 0,
-    [TransactionType.INVESTMENT]: transactionsTotal
-      ? Math.round((investmentsTotal / transactionsTotal) * 100)
-      : 0,
+    [TransactionType.DEPOSIT]: Math.round(
+      (Number(depositsTotal || 0) / Number(transactionsTotal)) * 100,
+    ),
+    [TransactionType.EXPENSE]: Math.round(
+      (Number(expensesTotal || 0) / Number(transactionsTotal)) * 100,
+    ),
+    [TransactionType.INVESTMENT]: Math.round(
+      (Number(investmentsTotal || 0) / Number(transactionsTotal)) * 100,
+    ),
   };
-
   const totalExpensePerCategory: TotalExpensePerCategory[] = (
     await db.transaction.groupBy({
       by: ["category"],
@@ -80,19 +72,16 @@ export const getDashboard = async (month: string) => {
     })
   ).map((category) => ({
     category: category.category,
-    totalAmount: Number(category._sum.amount) || 0,
-    percentageOfTotal:
-      expensesTotal > 0
-        ? Math.round((Number(category._sum.amount) / expensesTotal) * 100)
-        : 0,
+    totalAmount: Number(category._sum.amount),
+    percentageOfTotal: Math.round(
+      (Number(category._sum.amount) / Number(expensesTotal)) * 100,
+    ),
   }));
-
   const lastTransactions = await db.transaction.findMany({
     where,
     orderBy: { date: "desc" },
     take: 15,
   });
-
   return {
     balance,
     depositsTotal,
@@ -100,6 +89,6 @@ export const getDashboard = async (month: string) => {
     expensesTotal,
     typesPercentage,
     totalExpensePerCategory,
-    lastTransactions,
+    lastTransactions: JSON.parse(JSON.stringify(lastTransactions)),
   };
 };
